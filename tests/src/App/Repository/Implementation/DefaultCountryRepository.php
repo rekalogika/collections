fc6d6b21edeca530f020101b1dc41f5cd9338d5c
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-06-23 12:36:39 +0700

    feat: abstractrepository (#53)

---
+++ tests/src/App/Repository/Implementation/DefaultCountryRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/collections package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\Collections\Tests\App\Repository\Implementation;
+
+use Rekalogika\Collections\ORM\AbstractRepository;
+use Rekalogika\Collections\ORM\Configuration\RepositoryConfiguration;
+use Rekalogika\Collections\Tests\App\Repository\CountryRepository;
+use Rekalogika\Collections\Tests\App\Entity\Country;
+
+/**
+ * @extends AbstractRepository<array-key,Country>
+ */
+class DefaultCountryRepository extends AbstractRepository implements CountryRepository
+{
+    protected function configure(): RepositoryConfiguration
+    {
+        return new RepositoryConfiguration(
+            class: Country::class,
+        );
+    }
+}
