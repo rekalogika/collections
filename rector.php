60cc2b454395b77a576370bab27d1f251ef318d7
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-07-19 00:19:30 +0700

    finalize rector

--- rector.php
+++ rector.php
@@ -2,12 +2,10 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
 use Rector\CodeQuality\Rector\Identical\FlipTypeControlToUseExclusiveTypeRector;
 use Rector\CodeQuality\Rector\If_\CombineIfRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
 use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
-use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
@@ -16,8 +14,6 @@ use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
 use Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector;
 use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
-use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 use Rector\Strict\Rector\Ternary\DisallowedShortTernaryRuleFixerRector;
 
 return RectorConfig::configure()
@@ -34,10 +30,10 @@ return RectorConfig::configure()
         codingStyle: true,
         typeDeclarations: true,
         privatization: true,
-        // instanceOf: true,
-        // strictBooleans: true,
-        // symfonyCodeQuality: true,
-        // doctrineCodeQuality: true,
+        instanceOf: true,
+        strictBooleans: true,
+        symfonyCodeQuality: true,
+        doctrineCodeQuality: true,
     )
     ->withPhpSets(php82: true)
     ->withRules([
